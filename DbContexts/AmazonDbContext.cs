6aaa11b248cb3c7d00e5e5e2008226cb99ebb346
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-26 20:59:22 +0200

    Seed some products via onmodelcreating

--- DbContexts/AmazonDbContext.cs
+++ DbContexts/AmazonDbContext.cs
@@ -5,4 +5,35 @@ public class AmazonDbContext : DbContext
     { }
 
     public DbSet<ProductEntity> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ProductEntity>()
+            .HasData(
+                new ProductEntity
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    Price = 15000.5,
+                    CountInStock = 11
+                },
+                new ProductEntity
+                {
+                    Id = 2,
+                    Name = "Mobile",
+                    Price = 16000.5,
+                    CountInStock = 123
+                },
+                new ProductEntity
+                {
+                    Id = 3,
+                    Name = "Moza",
+                    Price = 13600.3,
+                    CountInStock = 43
+                }
+            );
+
+    }
 }
--- DbContexts/Migrations/20220826185841_SeededSomeProductsForTest.Designer.cs
+++ DbContexts/Migrations/20220826185841_SeededSomeProductsForTest.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace API.DbContexts.Migrations
 {
     [DbContext(typeof(AmazonDbContext))]
-    partial class AmazonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220826185841_SeededSomeProductsForTest")]
+    partial class SeededSomeProductsForTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
